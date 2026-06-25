168a47de1fab1193f4658e720508f18c7e9bae60
Rafee <rafeehasan88@gmail.com>
2026-06-26 00:24:16 +0600

    Setting Up Postgres with Neon Serverless Cloud

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
 import express, { type Application, type Request, type Response } from "express";
+import {Pool} from "pg";
 const app : Application = express(); //This invokes (calls) that function. When executed, it creates and configures a brand-new Express application object
 const port = 5000;
+
+app.use(express.json());
+
+const pool =  new Pool({
+  connectionString:"postgresql://neondb_owner:npg_WRlu8ieq9wXn@ep-spring-thunder-ahtmeuzt-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+})
 
 app.get('/', (req: Request, res: Response) => {
  // res.send('Hello World!');
@@ -10,7 +17,12 @@ app.get('/', (req: Request, res: Response) => {
   });
 });
 app.post('/' , async(req:Request , res:Response)=>{
-console.log(req);
+//console.log(req.body);
+const {name,id,password} = req.body;
+res.status(201).json({
+  message:"Post created",
+    data: {name, id},
+});
 });
 
 app.listen(port, () => {
