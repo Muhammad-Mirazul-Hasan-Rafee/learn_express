5fb047ef2bf158a1816f86c100d8cc15b2b3fe24
Rafee <rafeehasan88@gmail.com>
2026-06-28 00:23:58 +0600

    Delete using delete method . Set up Environment-based Configurations

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import express, { type Application, type Request, type Response } from "express";
 import { Pool } from "pg";
+import config from "./config";
 const app: Application = express(); //This invokes (calls) that function. When executed, it creates and configures a brand-new Express application object
-const port = 5000;
+const port = config.port;
 
 app.use(express.json());
 
 const pool = new Pool({
-  connectionString: "postgresql://neondb_owner:npg_WRlu8ieq9wXn@ep-spring-thunder-ahtmeuzt-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+  connectionString: config.CONNECTIVITYSTR,
 });
 
 const initDB = async () => {
@@ -107,30 +108,80 @@ app.get('/api/users/:id', async (req: Request, res: Response) => {
 
   }
 });
-app.put('/api/users/:id', async (req: Request, res: Response) => {
+// app.put("/api/users/:id", async (req: Request, res: Response) => {
+//   const id = Number(req.params.id);
+//   const { name, password } = req.body;
+
+//   try {
+//     const result = await pool.query(
+//       `
+//       UPDATE users
+//       SET
+//         name = COALESCE($1, name),
+//         password = COALESCE($2, password)
+//       WHERE id = $3
+//       RETURNING *;
+//       `,
+//       [name ?? null, password ?? null, id]
+//     );
+
+//     if (result.rows.length === 0) {
+//       return res.status(404).json({
+//         success: false,
+//         message: "User not found!",
+//         data: {},
+//       });
+//     }
+
+//     return res.status(200).json({
+//       success: true,
+//       message: "Updated successfully!",
+//       data: result.rows[0],
+//     });
+//   } catch (error: any) {
+//     return res.status(500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// });
+app.put("/api/users/:id", async (req: Request, res: Response) => {
   const { id } = req.params;
-  const { name, password } = req.body;
-  // console.log(id, name, password);
+  const { name, password, age, is_active } = req.body;
+
+  // console.log("Id : ", id);
+  // console.log({ name, password, age, is_active });
 
   try {
-    const result = await pool.query(`
-    UPDATE users SET name=$1, password=$2 WHERE id=$3 RETURNING *`, [name, password, id]);
-    //console.log(result);
+    const result = await pool.query(
+      `
+    UPDATE users 
+    SET 
+    name=COALESCE($1,name),
+    password=COALESCE($2,password),
+    age=COALESCE($3,age),
+    is_active=COALESCE($4,is_active) 
 
-    if(result.rows.length === 0){
-       res.status(404).json({
+    WHERE id=$5 RETURNING *
+    `,
+      [name, password, age, is_active, id],
+    );
+
+    if (result.rows.length === 0) {
+      res.status(404).json({
         success: false,
-        message: 'What to update? user not found! You are a bad programmer!! Get sleep...!',
-        data: {},
+        message: "User Not found!",
       });
     }
+
+    // console.log(result);
     res.status(200).json({
       success: true,
-      message: "updated successfully!!",
+      message: "User updated successfully!",
       data: result.rows[0],
     });
   } catch (error: any) {
-        res.status(500).json({
+    res.status(500).json({
       success: false,
       message: error.message,
       error: error,
@@ -138,7 +189,32 @@ app.put('/api/users/:id', async (req: Request, res: Response) => {
   }
 });
 
+app.delete('/api/users.:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const result = await pool.query(`
+      DELETE FROM users WHERE id=$1
+      `, [id]);
+      if(result.rowCount === 0){
+        res.status(404).json({
+        success: false,
+        message: "User Not found!",
+      });
+      }
+    res.status(200).json({
+      success: true,
+      message: "User deleted successfully!",
+      data: {},
+    });
+
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error,
+    });
+  }
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-// 9.43
